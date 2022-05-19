061df0d29a23a4db9f3e502088c4c6239e242059
ZakirAvrora <krasavasula@gmail.com>
2022-05-19 20:35:51 +0600

    init

--- reverse_string.go
+++ reverse_string.go
@@ -2,5 +2,16 @@ package reverse_string
 
 func ReverseString(input string) (output string) {
 	// solution goes here
-	return output
+	runes := []rune(input)
+
+	if len(runes) < 2 {
+		return input
+	}
+	i, j := 0, len(runes)-1
+
+	for i !=j {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+
+	return string(runes) 
 }
